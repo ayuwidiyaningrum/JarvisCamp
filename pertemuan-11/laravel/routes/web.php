dd52c27dc3d4e9afffc90fc21b050bda9928ea6f
ayuwidiyaningrum <ayuwidiya32@gmail.com>
2021-11-19 23:27:21 +0700

    pertemuan-11

---
+++ pertemuan-11/laravel/routes/web.php
@@ -0,0 +1,29 @@
+<?php
+
+use App\Http\Controllers\AuthController;
+use App\Http\Controllers\HomeController;
+use Illuminate\Support\Facades\Auth;
+use Illuminate\Support\Facades\Route;
+
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider within a group which
+| contains the "web" middleware group. Now create something great!
+|
+*/
+
+// Route::get('/', function () {
+//     return view('welcome');
+// });
+
+Route::get('/',[HomeController::class,'home']);
+Route::get('/register', [AuthController::class,'register']);
+Route::get('/welcome', [AuthController::class,'welcome']);
+
+
+
+
